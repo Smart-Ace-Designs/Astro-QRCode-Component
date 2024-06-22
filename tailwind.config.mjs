b646210a72481f6f31988237e63a265dc6c2000a
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2024-06-22 14:49:42 -0500

    Removes Major Tom default content and adds initial Tailwind CSS customizations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        qrWhite: "hsl(0, 0%, 100%)",
+        qrLightGray: "hsl(212, 45%, 89%)",
+        qrGrayishBblue: "hsl(220, 15%, 55%)",
+        qrDarkBlue: "hsl(218, 44%, 22%)",
+      },
+      fontFamily: {
+        qrBody: ["Outfit"],
+      },
+    },
   },
   plugins: [],
 };
