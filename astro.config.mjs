9895398f768c283e41fe943686246f6dc44b8ff4
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-03-22 23:51:00 -0500

    Fix favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://smart-ace-designs.github.io",
   base: "/Astro-QRCode-Component",
+  trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
   },
